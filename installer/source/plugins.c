875637c60dbbed8b9da5aaa762cfbab20d9b91a7
Al Azif <33132478+Al-Azif@users.noreply.github.com>
2025-07-05 19:17:19 -0700

    Chunk and zlib PRX files

--- installer/source/plugins.c
+++ installer/source/plugins.c
@@ -7,12 +7,12 @@
 // static won't redefine these symbols here
 #include "plugin_bootloader.prx.inc.c"
 #include "plugin_loader.prx.inc.c"
-#include "plugin_server.prx.inc.c"
 #include "plugin_mono.prx.inc.c"
+#include "plugin_server.prx.inc.c"
 
 void upload_prx_to_disk(void) {
-  write_blob(PRX_BOOTLOADER_PATH, plugin_bootloader_prx, plugin_bootloader_prx_len);
-  write_blob(PRX_LOADER_PATH, plugin_loader_prx, plugin_loader_prx_len);
-  write_blob(PRX_SERVER_PATH, plugin_server_prx, plugin_server_prx_len);
-  write_blob(PRX_MONO_PATH, plugin_mono_prx, plugin_mono_prx_len);
+  unchunk_decompress_and_write(plugin_bootloader_prx_chunks, plugin_bootloader_prx_chunk_lens, plugin_bootloader_prx_chunk_count, plugin_bootloader_prx_total_size, PRX_BOOTLOADER_PATH);
+  unchunk_decompress_and_write(plugin_loader_prx_chunks, plugin_loader_prx_chunk_lens, plugin_loader_prx_chunk_count, plugin_loader_prx_total_size, PRX_LOADER_PATH);
+  unchunk_decompress_and_write(plugin_mono_prx_chunks, plugin_mono_prx_chunk_lens, plugin_mono_prx_chunk_count, plugin_mono_prx_total_size, PRX_MONO_PATH);
+  unchunk_decompress_and_write(plugin_server_prx_chunks, plugin_server_prx_chunk_lens, plugin_server_prx_chunk_count, plugin_server_prx_total_size, PRX_SERVER_PATH);
 }
